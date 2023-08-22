cf73a12054fbd7815768e760b739e82245d315c2
Aleksei Anisimov <suxelshock@gmail.com>
2023-08-22 20:06:45 +0300

    Gridlayoutmanager

---
+++ app/src/main/java/ru/practicum/sprint10koh8/ListElement.kt
@@ -0,0 +1,48 @@
+package ru.practicum.sprint10koh8
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.ColorInt
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
+
+data class ListElement(
+    val id: String,
+    val name: String,
+    @ColorInt
+    val color: Int,
+) {
+    companion object {
+        fun createRandomElement(id: String): ListElement {
+            return ListElement(
+                id = id,
+                name = "ListElement $id",
+                color = Color.HSVToColor(
+                    arrayOf(
+                        Random.nextFloat().times(360),
+                        1.0f,
+                        1.0f
+                    ).toFloatArray()
+                )
+                // https://ru.wikipedia.org/wiki/HSV_(%D1%86%D0%B2%D0%B5%D1%82%D0%BE%D0%B2%D0%B0%D1%8F_%D0%BC%D0%BE%D0%B4%D0%B5%D0%BB%D1%8C)
+            )
+        }
+    }
+}
+
+class ListElementViewHolder(parentView: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parentView.context)
+        .inflate(R.layout.v_list_element, parentView, false)
+) {
+
+    private val image: View = itemView.findViewById(R.id.image)
+    private val text: TextView = itemView.findViewById(R.id.text)
+
+    fun bind(element: ListElement) {
+        image.setBackgroundColor(element.color)
+        text.text = element.name
+    }
+}
--- app/src/main/java/ru/practicum/sprint10koh8/MainActivity.kt
+++ app/src/main/java/ru/practicum/sprint10koh8/MainActivity.kt
@@ -1,11 +1,62 @@
 package ru.practicum.sprint10koh8
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val itemsRecyclerView: RecyclerView = findViewById(R.id.items)
+
+        val itemsAdapter = ItemsAdapter()
+
+        itemsRecyclerView.apply {
+            layoutManager = GridLayoutManager(
+                this@MainActivity, 3,
+                GridLayoutManager.VERTICAL, false
+            ).apply {
+                spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                    override fun getSpanSize(position: Int): Int {
+                        return if (position % 4 == 0) {
+                            3
+                        } else {
+                            1
+                        }
+                    }
+
+                }
+            }
+            adapter = itemsAdapter
+
+        }
+
+        val myItems = (1..20).map {
+            ListElement.createRandomElement(it.toString())
+        }
+
+        itemsAdapter.items.addAll(myItems)
     }
+}
+
+class ItemsAdapter : RecyclerView.Adapter<ListElementViewHolder>() {
+
+    var items: MutableList<ListElement> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListElementViewHolder {
+        return ListElementViewHolder(parent)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ListElementViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
 }
